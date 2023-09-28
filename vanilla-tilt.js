64e99b543d90639bc2663e3c9203505b566269dc
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2023-09-28 22:17:58 +0530

    index-main-style update

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -2,7 +2,7 @@ var VanillaTilt = (function () {
     'use strict';
     
     /**
-     * Created by Sergiu Șandor (micku7zu) on 1/27/2017.
+     * Created by Sergiu Șandor (micku7zu) on 10/27/2023.
      * Original idea: https://github.com/gijsroge/tilt.js
      * MIT License.
      * Version 1.8.0
